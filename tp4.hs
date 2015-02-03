64d9d238c66b1332798883010d0d0731915e17b6
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2015-02-03 11:03:38 +0100

    mmap

--- tp4.hs
+++ tp4.hs
@@ -4,3 +4,11 @@ Fichier contenant
 Matthieu Caron
 Arnaud Cojez
 -}
+
+--Q1
+data Arbre coul val = Vide | Cons coul val (Arbre coul val) (Arbre coul val) deriving Show
+
+--Q2
+mmap :: (valA -> valB) -> Arbre coul valA -> Arbre coul valB
+mmap _ Vide = Vide
+mmap f (Cons c v (ag) (ad)) = Cons c (f v) (mmap f ag) (mmap f ad)
